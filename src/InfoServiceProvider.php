15a5f87de1fa2adf49206e5ba2e1961b9e4918b7
M. Riedaa Gamieldien <61379972+gmlrie001@users.noreply.github.com>
2021-06-29 18:32:41 +0200

    Updated

--- src/InfoServiceProvider.php
+++ src/InfoServiceProvider.php
@@ -15,27 +15,23 @@ class InfoServiceProvider extends ServiceProvider
        * Optional methods to load your package assets
        */
       // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'info');
-      // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-      // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
+      $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+      $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
       $this->loadViewsFrom( __DIR__ . '/../resources/views', 'info' );
 
+      /*
       if ($this->app->runningInConsole()) {
-
         $this->publishes([
             __DIR__.'/../config/info.php' => config_path( 'info.php' ),
           ], 'config');
-
         // Publishing the views.
         /*$this->publishes([
           __DIR__.'/../resources/views' => resource_path( 'views/vendor/info' ),
         ], 'views' );*/
-
         // Publishing assets.
         /*$this->publishes([
           __DIR__.'/../resources/assets' => public_path( 'vendor/info' ),
         ], 'assets' );*/
-
         // Publishing the translation files.
         /*$this->publishes([
           __DIR__.'/../resources/lang' => resource_path( 'lang/vendor/info' ),
@@ -46,7 +42,7 @@ class InfoServiceProvider extends ServiceProvider
         //   Console\Commands\info::class
         // ]);
       }
-
+      */
     }
 
     /**
